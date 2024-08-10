d93be00200e9112fe3b2e9429d3c65dda2e00755
Bruna Santiago <brunasanthiago@outlook.com>
2024-08-10 16:13:37 -0300

    adicionando o skelleton

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ git clone git@github.com:bruna40/projeto-desenvolve.git
 
 ### Branchs
 
-- [main](https://github.com/bruna40/projeto-desenvolve/tree/main): onde fica a junção de todos os projetos
-- [1-projeto](https://github.com/bruna40/projeto-desenvolve/tree/1-projeto): onde fica o primeiro projeto do curso
-- [2-projeto](https://github.com/bruna40/projeto-desenvolve/tree/2-projeto): onde fia o segundo projeto do curso
+- [main](https://github.com/bruna40/projeto-desenvolve/tree/main): Branch principal contendo a versão final integrada dos projetos.
+- [1-projeto](https://github.com/bruna40/projeto-desenvolve/tree/1-projeto): Primeiro projeto do curso, consistindo em uma página única com HTML e CSS, focado em responsividade.
+- [2-projeto](https://github.com/bruna40/projeto-desenvolve/tree/2-projeto): Segundo projeto do curso, onde foi adicionado JavaScript para interação. A API de produtos foi incorporada para aprimorar a funcionalidade.
+- [3-projeto](https://github.com/bruna40/projeto-desenvolve/tree/2-projeto): Terceiro projeto do curso, que inclui o desenvolvimento do back-end para exibição de produtos. Foi implementada a paginação na API para melhorar o desempenho, resolvendo problemas de lentidão do segundo projeto. Confira o back-end [aqui](https://github.com/bruna40/back-projeto-desenvolve)
--- js/products.js
+++ js/products.js
@@ -1,4 +1,4 @@
-//const URL = 'http://localhost:3000/products';
+// const URL = 'http://localhost:3000/products';
 const URL = 'https://back-projeto-desenvolve.onrender.com/products';
 let productsPerPage = 5;
 let currentPage = 1;
@@ -6,9 +6,67 @@ let totalLoaded = 0;
 let totalProducts = 0;
 let allProducts = []; 
 
+
+function getProducts() {
+    const url = `${URL}?page=${currentPage}&limit=${productsPerPage}`;
+    showSkeleton();
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+
+            if (data.products && Array.isArray(data.products)) {
+                showProducts(data.products); 
+
+                totalProducts = data.pagination.totalProducts;
+                updateLoadMoreButton();
+
+                currentPage++;
+            } else {
+                throw new Error('Expected an array of products or an object containing an array of products');
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            document.getElementById('itens').innerHTML = 'There was an error fetching the data.';
+        })
+        .finally(() => {
+            hideSkeleton();
+        });
+}
+
+function showSkeleton() {
+    const contentDiv = document.getElementById('itens');
+    if (!contentDiv.querySelector('.skeleton-item')) {
+        const skeletonHTML = `
+            <div class="skeleton-item">
+                <div class="skeleton-title"></div>
+                <div class="skeleton-price"></div>
+                <div class="skeleton-button"></div>
+            </div>
+            <div class="skeleton-item">
+                <div class="skeleton-title"></div>
+                <div class="skeleton-price"></div>
+                <div class="skeleton-button"></div>
+            </div>
+            <div class="skeleton-item">
+                <div class="skeleton-title"></div>
+                <div class="skeleton-price"></div>
+                <div class="skeleton-button"></div>
+            </div>
+        `;
+        contentDiv.innerHTML = skeletonHTML;
+    }
+}
+
 function showProducts(products) {
     const contentDiv = document.getElementById('itens');
     const fragment = document.createDocumentFragment();
+    
 
     if (products.length === 0) {
         if (totalLoaded === 0) {
@@ -39,35 +97,13 @@ function showProducts(products) {
         });
         contentDiv.appendChild(fragment);
     }
+    
 }
 
-function getProducts() {
-    const url = `${URL}?page=${currentPage}&limit=${productsPerPage}`;
 
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-
-            if (data.products && Array.isArray(data.products)) {
-                showProducts(data.products); // Adiciona produtos à página
-
-                totalProducts = data.pagination.totalProducts;
-                updateLoadMoreButton();
-
-                currentPage++;
-            } else {
-                throw new Error('Expected an array of products or an object containing an array of products');
-            }
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            document.getElementById('itens').innerHTML = 'There was an error fetching the data.';
-        });
+function hideSkeleton() {
+    const skeletonItems = document.querySelectorAll('.skeleton-item');
+    skeletonItems.forEach(item => item.style.display = 'none');
 }
 
 
@@ -93,6 +129,7 @@ function searchProducts(searchTerm) {
                 showProducts(data.products);
                 allProducts = data.products;
                 totalProducts = data.pagination.totalProducts;
+                hideSkeleton();
                 updateLoadMoreButton();
                 currentPage++;
             } else {
